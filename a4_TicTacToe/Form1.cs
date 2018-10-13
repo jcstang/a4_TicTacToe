c6698988e0dbd9d2fb3bc03ddcd4e85c02e74ae9
Jacob Stanger <jacobcstanger@gmail.com>
2018-10-13 12:46:21 -0600

    added more ui elements

---
+++ a4_TicTacToe/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace a4_TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        clsTicTacToe clsTicTac;
+        bool bIsGameStarted;
+        string[,] saBoard;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            clsTicTac = new clsTicTacToe();
+            clsTicTac.saBoard = saBoard;
+            bIsGameStarted = false;
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            //Need to start the game or set bIsGameStarted variable
+            //clearLabels() and ResetColors()
+        }
+
+        //TODO: space clicked
+            //1. bIsGameStarted
+            //2. is space blank
+            //3. LoadBoard()
+            //4. clsTicTac.IsWinningMove()
+            //5. HighlightWinningMove() and DisplayScores()
+            //6. clsTicTac.IsTie()
+            //7. DisplayScores() [game over]
+
+
+        //TODO: lblSpace_Click(object sender, EventArgs e)
+            //cast the sender to label
+            //label myLabel = (Label)sender;
+            //mylabel.text = "x";
+            //setBackgroundColor(myLabel);
+
+
+        //TODO: Create ClearLabels() and a ResetColors()
+        //TODO: LoadBoard()
+        //TODO: SetBackgroundColor(Label lblLabel)
+            //lblLabel.BackColor = Color.Yellow;
+
+    }
+}
---
+++ a4_TicTacToe/clsTicTacToe.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace a4_TicTacToe
+{
+    class clsTicTacToe
+    {
+        public string[,] saBoard;
+        private int iPlayer1Wins;
+        private int iPlayer2Wins;
+        private int iTies;
+        private WinningMove eWinningMove;
+
+        private enum WinningMove
+        {
+            Row1,
+            Row2,
+            Row3,
+            Col1,
+            Col2,
+            Col3,
+            Diag1,
+            Diag2
+        }
+
+        public bool IsWinningMove()
+        {
+            eWinningMove = WinningMove.Row2;
+        }
+        private bool IsDiagonalWin()
+        {
+
+        }
+        private bool IsVertWin()
+        {
+
+        }
+        private bool IsHorzWin()
+        {
+
+        }
+        public bool IsTie()
+        {
+
+        }
+
+    }
+}
